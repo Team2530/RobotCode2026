878cbac15cd4d12d6b8f9538875d51028454e861
CNrailfaner4039 <leviahar4039@gmail.com>
2026-01-21 20:06:32 -0600

    Subsystem -> Command migration

--- src/main/java/frc/robot/commands/ClimberCommand.java
+++ src/main/java/frc/robot/commands/ClimberCommand.java
@@ -1,30 +1,38 @@
 package frc.robot.commands;
 
+import com.revrobotics.spark.SparkFlex;
+
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants;
 import frc.robot.Robot;
 import frc.robot.subsystems.ClimberSubsystem;
 import frc.robot.subsystems.ClimberSubsystem.ClimbPresets;
 
 public class ClimberCommand extends Command {
     private final ClimberSubsystem subsystem1;
+    private final ClimberCommand command1;
 
-    
+    private final SparkFlex m_climbsubsystemMotor;
+    private ClimbPresets climbPreset;
 
     public ClimberCommand(ClimberSubsystem subsystem1) {
-        this.subsystem1 = subsystem1;
-        addRequirements(subsystem1);
+        this.subsystem1 = command1;
+        addRequirements();
     }
 
      @Override
     public void end(boolean interrupted) {
-        subsystem1.setIntakePreset(ClimbPresets.IDLE);
+        subsystem1.setClimbPreset(ClimbPresets.IDLE);
         SmartDashboard.putString("Climb Ended", "Ended");
     }
 
      @Override
     public void initialize() {
-        subsystem1.setIntakePreset(ClimbPresets.CLIMBING);
+        subsystem1.setClimbPreset(ClimbPresets.CLIMBING);
         SmartDashboard.putString("Climb Command", "Started");
         
 
@@ -33,4 +41,44 @@ public class ClimberCommand extends Command {
         
         }
     }
+    public void setupPreset(ClimberSubsystem.ClimbPresets climb) {
+        this.climbPreset = climb;
+        boolean climbActive = climb == ClimbPresets.CLIMBING;
+        boolean climbInactive = climb == ClimbPresets.IDLE;
+        if (ClimbPresets.CLIMBING == climb) {
+
+          m_climbsubsystemMotor.set(Constants.ClimbMotors.CLIMB_SPEED);
+          SmartDashboard.putString("Climbing Subsystem", "Begin");
+
+          new WaitCommand(Constants.Sleep.first); //TODO: find a way to make timer, DONE
+
+          m_climbsubsystemMotor.set(0);
+          //wait for button press. if auto, wait some time.
+          if (DriverStation.isAutonomousEnabled()) {
+            new WaitCommand(Constants.Sleep.first);
+          }
+          else {
+            //Button press
+          }
+          m_climbsubsystemMotor.set(-Constants.ClimbMotors.CLIMB_SPEED);
+
+          new WaitCommand(Constants.Sleep.last); 
+
+          SmartDashboard.putString("Climbing Command", "Finished");
+          
+  
+          if (Robot.isSimulation()) {
+              SmartDashboard.putBoolean("[SIM] Climbing", true);
+          
+          }
+        }
+        if (SmartDashboard.putString("Climbing Command", "Finished")) {
+          m_climbsubsystemMotor.set(0);
+          SmartDashboard.putString("Climbing Command", "Ended");
+              if (Robot.isSimulation()) {
+              SmartDashboard.putBoolean("[SIM] Climbing", false);
+          
+          }
+        }
+      }
 }
--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -12,61 +12,18 @@ import frc.robot.Robot;
 
 public class ClimberSubsystem extends SubsystemBase {
     
-    private final SparkFlex m_climbsubsystemMotor;
+    private final SparkFlex m_climbcommandMotor;
     private ClimbPresets climbPreset;
 
     public ClimberSubsystem() {
-        m_climbsubsystemMotor = new SparkFlex(Constants.ClimbMotors.CLIMBSUBSYSTEM_MOTOR, MotorType.kBrushless);//TODO: Change to ACTUAL motor type
+        m_climbcommandMotor = new SparkFlex(Constants.ClimbMotors.CLIMBSUBSYSTEM_MOTOR, MotorType.kBrushless);//TODO: Change to ACTUAL motor type
               
     }
 
     public void runIntake() {
-        m_climbsubsystemMotor.set(Constants.ClimbMotors.CLIMB_SPEED);
+        m_climbcommandMotor.set(Constants.ClimbMotors.CLIMB_SPEED);
             
     }
-    public void setIntakePreset(ClimbPresets climb) {
-        this.climbPreset = climb;
-        boolean climbActive = climb == ClimbPresets.CLIMBING;
-        boolean climbInactive = climb == ClimbPresets.IDLE;
-        if (ClimbPresets.CLIMBING == climb) {
-
-          m_climbsubsystemMotor.set(Constants.ClimbMotors.CLIMB_SPEED);
-          SmartDashboard.putString("Climbing Subsystem", "Begin");
-
-          new WaitCommand(Constants.Sleep.first); //TODO: find a way to make timer, DONE
-
-          m_climbsubsystemMotor.set(0);
-          //wait for button press. if auto, wait some time.
-          if (DriverStation.isAutonomousEnabled()) {
-            new WaitCommand(Constants.Sleep.first);
-          }
-          else {
-            //Button press
-          }
-          m_climbsubsystemMotor.set(-Constants.ClimbMotors.CLIMB_SPEED);
-
-          new WaitCommand(Constants.Sleep.last); 
-
-          SmartDashboard.putString("Climbing Subsystem", "Finished");
-          
-  
-          if (Robot.isSimulation()) {
-              SmartDashboard.putBoolean("[SIM] Climbing", true);
-          
-          }
-        }
-        if (SmartDashboard.putString("Climbing Subsystem", "Finished")) {
-          m_climbsubsystemMotor.set(0);
-          SmartDashboard.putString("Climbing Subsystem", "Ended");
-          
-  
-          if (Robot.isSimulation()) {
-              SmartDashboard.putBoolean("[SIM] Climbing", false);
-          
-          }
-        }
-      }
-      
          
       public ClimbPresets getClimbPreset() {
         return this.climbPreset;
