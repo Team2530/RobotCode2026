3df6d34865dca3f96b29e7d748286a580e102546
spoopr <spoopr@nootr.dev>
2026-01-28 19:15:07 -0600

    change controller ports

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -37,8 +37,8 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
  */
 public final class Constants {
   public static class ControllerConstants {
-    public static final int DRIVER_CONTROLLER_PORT = 1;
-    public static final int OPERATOR_CONTROLLER_PORT = 2;
+    public static final int DRIVER_CONTROLLER_PORT = 0;
+    public static final int OPERATOR_CONTROLLER_PORT = 1;
   }
 
   public static class RobotConstants {
