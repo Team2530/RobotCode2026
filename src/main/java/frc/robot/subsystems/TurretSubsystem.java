bdfa794743b9cbe3d400fee6de08eb6193ac23b3
MukhilAnanth <mukhil.ananth@gmail.com>
2026-01-30 11:04:27 -0600

    Installed Apache Math Commons

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -6,6 +6,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.CAN;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -20,6 +21,7 @@ import com.revrobotics.spark.SparkMax;
 import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.Robot;
+import org.apache.commons.math3.fitting.leastsquares.*;
 
 public class TurretSubsystem extends SubsystemBase {
     private static final int LauncherID = Constants.TurretConstants.FlyWheelID;
