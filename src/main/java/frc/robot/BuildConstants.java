d35b3b56b7ef68d0c9bbcf48b304867bd234f8e2
CNrailfaner4039 <leviahar4039@gmail.com>
2026-01-31 09:15:05 -0600

    E file adding :>

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -4,12 +4,12 @@ package frc.robot;
  * Automatically generated file containing build version information.
  */
 public final class BuildConstants {
-    public static final String MAVEN_GROUP = "";
+    public static final String MAVEN_GROUP = "2530";
     public static final String MAVEN_NAME = "RobotCode2026";
     public static final String VERSION = "ThE mOsT rEcEnT oNe";
     public static final int GIT_REVISION = 5;
     public static final String GIT_SHA = "bccd7eb881eba8a2c445853e5af72829a1aaac19";
-    public static final String GIT_DATE = "2026-01-10 09:47:49 CST";
+    public static final String GIT_DATE = "2026-01-30 09:14:49 CST";
     public static final String GIT_BRANCH = "Climber";
     public static final String BUILD_DATE = "2026-01-12 20:55:55 CST";
     public static final long BUILD_UNIX_TIME = 1768272955275L;
