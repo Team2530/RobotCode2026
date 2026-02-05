2b6c9a337f69a0c7a0467aad021b2428d99e9e61
chezerguy <thehitbox123@gmail.com>
2026-02-04 20:27:02 -0600

    Merge branch 'main' into BUTTON-PRESS-ROBOT-DO-SOMETHING

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.Subsystem;
@@ -83,14 +84,21 @@ public class RobotContainer {
 
         // NamedCommands.registerCommand(null, getAutonomousCommand());
     }
-
+    private boolean intakeDown = false;
     
-
+    //TODO: Fix this with grouped commands 
     private Command getIntakingCommand() {
-        return new ParallelCommandGroup(
-            new IntakeCommand(intakeSubsystem)
-            //putting down the intake goes here
-        );
+        //if (!intakeDown){
+            intakeDown = true;
+            return new ParallelCommandGroup(
+                new IntakeCommand(intakeSubsystem)
+                //intake down here
+            );
+        //}
+        
+    
+       
+        
     }
 
     private Command getShootingCommand() {
@@ -140,3 +148,4 @@ public class RobotContainer {
         return operatorXbox;
     }
 }
+    
