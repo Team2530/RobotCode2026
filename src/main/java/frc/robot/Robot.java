22aed3f6514d298af391ea23336809d88ba3f1eb
chezerguy <thehitbox123@gmail.com>
2026-02-05 19:39:14 -0600

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -51,7 +51,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   
-  @Logged
+  
   private RobotContainer m_robotContainer;
 
   double lastLoopTime = Timer.getFPGATimestamp();
