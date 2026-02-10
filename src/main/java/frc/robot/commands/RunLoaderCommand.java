9dd740d3b2367457285b8d0c55446cdcb655bcc5
spoopr <spoopr@nootr.dev>
2026-02-09 19:18:07 -0600

    add `commands/RunLoaderCommand.java`

---
+++ src/main/java/frc/robot/commands/RunLoaderCommand.java
@@ -0,0 +1,29 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.LoaderSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class RunLoaderCommand extends Command {
+    private final LoaderSubsystem loaderSubsystem;
+
+    public RunLoaderCommand(IndexerSubsystem loaderSubsystem) {
+        this.loaderSubsystem = loaderSubsystem;
+        addRequirements(loaderSubsystem);
+    }
+    
+    @Override
+    public void initialize() {
+        loaderSubsystem.run();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        loaderSubsystem.stop();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
+
