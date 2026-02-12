a14b773239fb953b95625d5dbe9084648b10bc96
spoopr <spoopr@nootr.dev>
2026-02-11 22:27:59 -0600

    turret remote merge changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -358,6 +358,8 @@ public final class Constants {
     public static final class Pitch {
         public static final double GEAR_RATIO = 1;
 
+        public static final double ANGLE_CONSTANT = 70.5;
+
         // 90 would have the "face" of the turret as vertical
         // 0 would have the "face" be horizontal (outputting up)
         /* 
