bc840eeffdd4388f1c98a408aa2d5c0218416796
EmulatorDotEXE <collin.litzinger@gmail.com>
2026-02-16 15:08:32 -0600

    adding some choreo functionality

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,20 +4,27 @@
 
 package frc.robot;
 
-//import java.util.function.Consumer;
-//import com.pathplanner.lib.auto.AutoBuilder;
+import java.util.function.Consumer;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
+import choreo.auto.AutoChooser;
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+
 import frc.robot.Constants.ControllerConstants;
 import frc.robot.commands.DriveCommand;
 import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.util.AllianceFlipUtil;
 import frc.robot.subsystems.Limelight.LimelightType;
 import frc.robot.util.LimelightContainer;
 import frc.robot.subsystems.Limelight;
@@ -31,29 +38,26 @@ import frc.robot.subsystems.Limelight;
  * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
-//@Logged(strategy = Logged.Strategy.OPT_IN)
+@Logged(strategy = Logged.Strategy.OPT_IN)
 public class RobotContainer {
-    // These are initating the individual Limlight(s)
-     private static final Limelight LL_BR = new Limelight(LimelightType.LL4, "limelight-br", true, true);
-     private static final Limelight LL_FR = new Limelight(LimelightType.LL4, "limelight-fr", true, true);
-     private static final Limelight LL_EX = new Limelight(LimelightType.LL4, "limelight-ex", true, true);
-     private static final Limelight LL_BF = new Limelight(LimelightType.LL4, "limelight-bf", true, true);
+    // These are initating the individual Limlight(s). The name should match the limelight internal names.
+    private static final Limelight LL_BR = new Limelight(LimelightType.LL4, "limelight-br", true, true);
+    private static final Limelight LL_FR = new Limelight(LimelightType.LL4, "limelight-fr", true, true);
+    private static final Limelight LL_BL = new Limelight(LimelightType.LL4, "limelight-bl", true, true);
+    private static final Limelight LL_BF = new Limelight(LimelightType.LL4, "limelight-bf", true, true);
 
-     //initalizing limelight container (Group)
-     public static final LimelightContainer LLContainer = new LimelightContainer(LL_BF, LL_EX, LL_BR, LL_FR);
-
+    //initalizing limelight container (Group)
+    public static final LimelightContainer LLContainer = new LimelightContainer(LL_BF, LL_BL, LL_BR, LL_FR);
     // @Logged
-    public final CommandXboxController driverXbox = new CommandXboxController(
-            ControllerConstants.DRIVER_CONTROLLER_PORT);
+    public final CommandXboxController driverXbox = new CommandXboxController(ControllerConstants.DRIVER_CONTROLLER_PORT);
     // @Logged
-    public final CommandXboxController operatorXbox = new CommandXboxController(
-            ControllerConstants.OPERATOR_CONTROLLER_PORT);
-
-    private final SendableChooser<Command> autoChooser = new SendableChooser<>();
+    public final CommandXboxController operatorXbox = new CommandXboxController(ControllerConstants.OPERATOR_CONTROLLER_PORT);
 
     @Logged
     public final SwerveSubsystem swerveDriveSubsystem = new SwerveSubsystem();
 
+    // Autonomous chooser
+    private final AutoChooser autoChooser = new AutoChooser();
     // private final LimeLightSubsystem limeLightSubsystem = new
     // LimeLightSubsystem();
     @Logged
@@ -63,6 +67,8 @@ public class RobotContainer {
      * The container for the robot. Contains subsystems, OI devices, and commands.
      */
     public RobotContainer() {
+
+        
         // Configure the trigger bindings
         configureBindings();
 
@@ -74,10 +80,7 @@ public class RobotContainer {
         swerveDriveSubsystem.setDefaultCommand(normalDrive);
 
         // NamedCommands.registerCommand(null, getAutonomousCommand());
-
-    
     }
-
 
     
 
@@ -107,7 +110,7 @@ public class RobotContainer {
      * @return the command to run in autonomous
      */
     public Command getAutonomousCommand() {
-        return autoChooser.getSelected();
+        return autoChooser.selectedCommand();
     }
 
     public SwerveSubsystem getSwerveSubsystem() {
