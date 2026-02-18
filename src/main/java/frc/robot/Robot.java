4aa261540fde0fa198d4524c4a84a2f4846aa016
MukhilAnanth <mukhil.ananth@gmail.com>
2026-02-18 17:54:50 -0600

    Fixed phoenix imports in robot.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -24,6 +24,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
+import com.ctre.phoenix6.Orchestra;
+import com.ctre.phoenix6.hardware.TalonFX;
+
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to
