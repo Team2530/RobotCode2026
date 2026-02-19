18947efc835d31ed2a5cf862f47ba803496dc90b
EmulatorDotEXE <collin.litzinger@gmail.com>
2026-02-18 18:37:50 -0600

    changed?

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -29,6 +29,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.Constants.DriveConstants.PIDs.Drive;
+import frc.robot.subsystems.SwerveSubsystem;
 import swervelib.SwerveDrive;
 import choreo.Choreo;
 import choreo.auto.AutoFactory;
@@ -46,7 +47,7 @@ import choreo.auto.AutoFactory;
 public class Robot extends TimedRobot {
 
   private Command m_autonomousCommand;
-  private final Drive swerveDrive = new Drive();
+  private final SwerveSubsystem swerveDrive = new SwerveSubsystem();
   @Logged
   private RobotContainer m_robotContainer;
 
