dc717f5e420623f07d77030b9778dc627e757e4a
spoopr <spoopr@nootr.dev>
2026-02-20 17:46:45 -0600

    zero turret on autonomous init

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -149,7 +149,12 @@ public class Robot extends TimedRobot {
 
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
-       CommandScheduler.getInstance().schedule(m_autonomousCommand);
+       CommandScheduler.getInstance().schedule(
+          new ParallelCommandGroup(
+            m_autonomousCommand,
+            m_robotContainer.getInitCommand() 
+          )
+        );
     }
 
     Elastic.selectTab("Autonomous");
