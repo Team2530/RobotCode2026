51a8005e35c4d0cb6570e803c96be6b1c8ea5fb3
spoopr <spoopr@nootr.dev>
2026-02-20 18:16:10 -0600

    forgot import in `Robot.java`

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -23,6 +23,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.util.Elastic;
 import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.Limelight.LimelightType;
