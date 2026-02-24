ced338275b6a6eb60e8657c565c46fe421690bb6
spoopr <spoopr@nootr.dev>
2026-02-23 20:04:40 -0600

    bugfix loader command

--- src/main/java/frc/robot/commands/RunLoaderCommand.java
+++ src/main/java/frc/robot/commands/RunLoaderCommand.java
@@ -21,10 +21,10 @@ public class RunLoaderCommand extends Command {
     @Override
     public void initialize() {
         if (m_Reversed){
-            loaderSubsystem.runReverse();
+            loaderSubsystem.runVelocity(-.6);
         }
         else{
-            loaderSubsystem.run();
+            loaderSubsystem.runVelocity(.6);
         }
     }
 
