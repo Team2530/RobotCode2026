518120e16d90a8d7c3be6d3417ac7795fbac9e9a
chezerguy <thehitbox123@gmail.com>
2026-02-23 19:12:00 -0600

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -23,8 +23,12 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.util.Elastic;
+import frc.robot.commands.IntakeCommand;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.IntakeSubsystem.IntakePreset;
 import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.Limelight.LimelightType;
 import frc.robot.util.LimelightHelpers;
@@ -133,7 +137,12 @@ public class Robot extends TimedRobot {
 
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+    new IntakeCommand(new IntakeSubsystem(), IntakePreset.INTAKING, IntakePreset.STOWED).schedule();
+    CommandScheduler.getInstance().cancelAll();
+    
+    Elastic.selectTab("Disabled");
+  }
 
   @Override
   public void disabledPeriodic() {
