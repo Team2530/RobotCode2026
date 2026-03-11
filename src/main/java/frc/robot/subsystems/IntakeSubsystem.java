fdc625e2fedeb08f9d485e7d694eca9a71ee4336
spoopr <spoopr@nootr.dev>
2026-03-11 18:56:11 -0500

    run feeder on agitate

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -35,7 +35,7 @@ public class IntakeSubsystem extends SubsystemBase {
         STOWED(true, 0),
         OUT(false, 0),
         INTAKING(false, 44), //intake speed, in rps
-        AGITATING(false, 0),
+        AGITATING(false, 40), 
         SPITTING(false, -IntakeConstants.Feeder.MAXIMUM_VELOCITY),
         CUSTOM(false, Double.MAX_VALUE);
 
