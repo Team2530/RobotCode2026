f34686ec8f87b739f4004e390ca7813de8fbbf0c
spoopr <spoopr@nootr.dev>
2026-03-22 22:31:23 -0500

    add missing error variable

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -475,6 +475,8 @@ public final class Constants {
             // public static final double ANGLE_MIN = 0;
             public static final Angle ANGLE_MAX = Degrees.of(340);
 
+            public static final Angle MAXIMUM_ANGLE_ERROR = Degrees.of(10);
+
             public static final AngularVelocity MAX_VELOCITY =
                 RotationsPerSecond.of(200);
             public static final AngularAcceleration MAX_ACCELERATION =
